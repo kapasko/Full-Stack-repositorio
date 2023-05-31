017bda9fd4b4d0bd9efa294c502479b6a7d653c4
Jaakko Tossavainen <jaakkotos.altti@gmail.com>
2023-05-31 19:11:47 +0300

    tehty unicafe-tehtävä

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -10,23 +10,70 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const StatisticLine = ({ text1, text2, value }) => (
+  <tr>
+    <td>{text1}</td>
+    <td>{value} {text2}</td>
+  </tr>
+)
+
+const Statistics = ({ good, neutral, bad, all, sum, positive }) => {
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text1="good" value={good} />
+        <StatisticLine text1="neutral" value={neutral} />
+        <StatisticLine text1="bad" value={bad} />
+        <StatisticLine text1="all" value={all} />
+        <StatisticLine text1="average" value={sum / all} />
+        <StatisticLine text1="positive" value={(positive / all) * 100} text2="%" />  
+      </tbody>
+    </table>
+  )
+}
+
+const Feedback = ({ good, neutral, bad, all, sum, positive }) => {
+  if (all === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} sum={sum} positive={positive} />
+    </div>
+  )
+}
+
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [sum, setSum] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleGood = () => (
+  const handleGood = () => {
     setGood(good + 1)
-  )
+    setAll(all + 1)
+    setSum(sum + 1)
+    setPositive(positive + 1)
+  }
 
-  const handleNeutral = () => (
+  const handleNeutral = () => {
     setNeutral(neutral + 1)
-  )
+    setAll(all + 1)
+  }
 
-  const handleBad = () => (
+  const handleBad = () => {
     setBad(bad + 1)
-  )
+    setAll(all + 1)
+    setSum(sum - 1)
+  }
 
   return (
     <div>
@@ -35,9 +82,7 @@ const App = () => {
       <Button handleClick={handleNeutral} text="neutral" />
       <Button handleClick={handleBad} text="bad" />
       <Header text="statistics" />
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Feedback good={good} neutral={neutral} bad={bad} all={all} sum={sum} positive={positive} />
     </div>
   )
 }
